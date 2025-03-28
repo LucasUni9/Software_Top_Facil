d05089b5981728f5da6a26474aa0925ce009c335
LucasUni9 <lucas.costa.souza@uni9.edu.br>
2025-03-28 15:53:57 -0300

    Método criar tabela tarefa

--- src/connection/ControleConexao.java
+++ src/connection/ControleConexao.java
@@ -24,7 +24,7 @@ public class ControleConexao {
 	//Criar tabela usuario
 	public static void criarTabelaUsuario() throws SQLException, IOException {
 		String sql = """
-				CREATE TABLE usuarios (
+				CREATE TABLE IF NOT EXISTS usuarios (
 				id INT AUTO_INCREMENT PRIMARY KEY, 
 				nome VARCHAR(100) NOT NULL,
 				email VARCHAR(100) UNIQUE NOT NULL, 
@@ -40,18 +40,25 @@ public class ControleConexao {
 	}
 
 	//Criar Tabela tarefa
- /* public static void criarBanco() throws SQLException, IOException {
+ public static void criarTabelaTarefa throws SQLException, IOException {
 		String sql = """
-				CREATE DATABASE IF NOT EXISTS topFacil
+				CREATE TABLE IF NOT EXISTS tarefas (
+				id INT AUTO_INCREMENT PRIMARY KEY,
+				nome VARCHAR(100) NOT NULL,
+				descricao TEXT,
+				status ENUM('pendente', 'em andamento', 'concluída') DEFAULT 'pendente',
+				usuario_id INT,
+				FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+				);
 				""";
 
 		Connection conexao = Conexao.conectarBanco();
 		Statement stmt = conexao.createStatement();
 		stmt.execute(sql);
-		System.out.println("Banco de dados criado com sucesso!");
+		System.out.println("Tabela Tarefa criado com sucesso!");
 		
 		conexao.close();
-	} */
+	}
 
 	//adicionar usuario
 	/*public static void criarBanco() throws SQLException, IOException {
