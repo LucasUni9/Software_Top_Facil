d361d8b9a9eb922869b8683fd1691e4c310c90c1
LucasUni9 <lucas.costa.souza@uni9.edu.br>
2025-03-28 15:47:33 -0300

    Método criar tabela usuário

--- src/connection/ControleConexao.java
+++ src/connection/ControleConexao.java
@@ -21,20 +21,25 @@ public class ControleConexao {
 		conexao.close();
 	}
 	
-	//Criar tabela public static void criarBanco() throws SQLException, IOException {
-	/*String sql = """
-				CREATE DATABASE IF NOT EXISTS topFacil
+	//Criar tabela usuario
+	public static void criarTabelaUsuario() throws SQLException, IOException {
+		String sql = """
+				CREATE TABLE usuarios (
+				id INT AUTO_INCREMENT PRIMARY KEY, 
+				nome VARCHAR(100) NOT NULL,
+				email VARCHAR(100) UNIQUE NOT NULL, 
+				senha VARCHAR(255) NOT NULL );
 				""";
 
 		Connection conexao = Conexao.conectarBanco();
 		Statement stmt = conexao.createStatement();
 		stmt.execute(sql);
-		System.out.println("Banco de dados criado com sucesso!");
+		System.out.println("Tabela usuarios criado com sucesso!");
 		
 		conexao.close();
-	} */
+	}
 
-	//Criar Tabela usuario
+	//Criar Tabela tarefa
  /* public static void criarBanco() throws SQLException, IOException {
 		String sql = """
 				CREATE DATABASE IF NOT EXISTS topFacil
